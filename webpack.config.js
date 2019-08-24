6b86902e310af4763eaf354fce2a14a2ef36c146
劉信俞 <luke@liuxinyudeMacBook-Pro.local>
2019-08-24 17:41:29 +0800

    set basic project structure

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack")
+const path = require('path')
+const APP_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+
+const config = {
+    entry: APP_DIR + '/app.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: 'app.js'
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+		}]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devtool: 'source-map',
+	devServer: {
+		inline: true,
+		contentBase: BUILD_DIR,
+		port: 8008,
+		hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+}
+
+module.exports = config
